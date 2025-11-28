24861096599efd7bdd519cd11ce87fb6268cd961
sekerate231 <abdujalilmannapov@gmail.com>
2025-11-28 18:45:53 +0500

    Add files via upload

---
+++ greet/main.py
@@ -0,0 +1,84 @@
+# # 1
+# class Car:
+#     def __init__(self,model,year):
+#         self.model=model
+#         self.year=year
+#     def __str__(self):
+#         return f'mashinani modeli {self.model} va yili {self.year}'
+# car1=Car('La ferarri',2020)
+# print(car1.model)
+# print(car1.year)
+# print(car1)
+# # 2
+# class Student:
+#     def __init__(self,name,age):
+#         self.ism=name
+#         self.age=age
+#     def __str__(self):
+#         return f'studentni ismi {self.ism} va yoshi {self.age}'
+# student1=Student("Abdujalil",15)
+# print(student1.ism)
+# print(student1.age)
+# print(student1)
+# # 3
+# class Retangle:
+#     def __init__(self,uzunlik,kenlik):
+#         self.uzun=uzunlik
+#         self.ken=kenlik
+#     def __str__(self):
+#         return f'uzunliki {self.uzun} va kenligi {self.ken}'
+#     def area(self):
+#         return  self.uzun * self.ken
+# ret=Retangle(14,15)
+# print(ret.uzun)
+# print(ret.ken)
+# print(ret)
+# print(ret.area()) 
+# # 4
+# class Phone:
+#     def __init__(self,model,xotira):
+#         self.model=model
+#         self.xotira=xotira
+#     def __str__(self):
+#         return f'Telefon yaratildi! {self.model}'
+# phone=Phone('Redmagic','1tb')
+# print(phone.model)
+# print(phone.xotira)
+# print(phone)
+# # 5
+# class Bank:
+#     def __init__(self,balans):
+#         self.balans=balans
+#     def __str__(self):
+#         return 'Balans ochildi!'
+# bank1=Bank(234000)
+# print(bank1.balans)
+# print(bank1)
+# 6
+# class Srote:
+#     tax=12
+#     def __init__(self,nom,soni):
+#         self.nom=nom
+#         self.nom
+# # 7
+# class School:
+#     country='Uzbekistan'
+#     def __init__(self,oquchilar):
+#         self.oquvchilar=oquchilar
+#     def __str__(self):
+#         return f'{self.oquvchilar } oquvchilarni davlati {self.country}'
+# school1=School('rus')
+# print(school1.oquvchilar)
+# print(school1)
+# # 8
+# class Worker:
+#     def __init__(self,ism,lavozim):
+#         self.ism=ism
+#         self.lavozim=lavozim
+#     def __str__(self):
+#         return f' {self.ism}ning lavozimi {self.lavozim}'
+# worker1=Worker('Andrey','Manager')
+# print(worker1.ism)
+# print(worker1.lavozim)
+# print(worker1)
+# 9
